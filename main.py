ae4c02df1061a4a93c9625b4d284c682edddc9ed
Dmitry286 <dmishi@live.spbstu.ru>
2022-09-12 16:08:11 +0300

    - Request class - отправка запросов на url - отправка запросов в java-приложение

--- main.py
+++ main.py
@@ -3,49 +3,24 @@ import json
 from pandas.io.json import json_normalize
 
 from CosineSimilarity import CosineSimilarity
+from Request import Request
 
 if __name__ == '__main__':
 
-    # CURRENT_USER = 'ivan'
-    # FILE_NAME = "C:\\Users\\Vladimir\\Python\\pythonProject\\recommendation_module\\venv\\data.csv"
-    user_ivan_comparison = CosineSimilarity('ivan', "C:\\Users\\Vladimir\\Python\\pythonProject\\recommendation_module\\venv\\data.csv")
+    # user_ivan_comparison = CosineSimilarity('ivan', "C:\\Users\\Vladimir\\Python\\pythonProject\\recommendation_module\\venv\\data.csv")
+    #
+    # empty_matrix = user_ivan_comparison.empty_matrix_creating()
+    # CosineSimilarity.print_matrix(empty_matrix)
+    #
+    # filled_matrix = user_ivan_comparison.matrix_filling()
+    # CosineSimilarity.print_matrix(filled_matrix)
+    # user_ivan_comparison.print_user_table(filled_matrix)
+    #
+    # weight_list = user_ivan_comparison.get_final_weight_for_each_product(filled_matrix)
+    # user_ivan_comparison.define_recommended_product(weight_list)
 
-    empty_matrix = user_ivan_comparison.empty_matrix_creating()
-    user_ivan_comparison.print_matrix(empty_matrix)
+    new_request = Request('https://dummyjson.com/carts')
+    new_request.make_json_from_data()
 
-    filled_matrix = user_ivan_comparison.matrix_filling()
-    user_ivan_comparison.print_matrix(filled_matrix)
-    user_ivan_comparison.print_user_table(filled_matrix)
-    # CosineSimilarity.print_matrix()
-
-    weight_list = user_ivan_comparison.get_final_weight_for_each_product(filled_matrix)
-    user_ivan_comparison.define_recommended_product(weight_list)
-
-
-    with open("C:\\Users\\Vladimir\\Python\\pythonProject\\recommendation_module\\orders.txt", "r", encoding='utf-8') as f:
-        data = json.load(f)
-        data = json_normalize(data)
-        df = pd.DataFrame(data)
-        print(df)
-        for i in df:
-            print("i from df")
-            print(i)
-
-        print(df["products"])
-        print(df["products"][0])
-
-        print(df["id"])
-        print(df["id"][0])
-
-        list_ = []
-        for i in df["products"][0]:
-            print(i['id'])
-            list_.append(i['id'])
-            print(list_)
-
-        list_[0] = 111
-        print(list_)
-
-
-
-
+    rest_api_request = Request('http://localhost:8080/order-data')
+    rest_api_request.test_request()
