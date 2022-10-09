1d7844098ec497f67b26b19e2d1ab09af3d00e55
Dmitry286 <dmishi@live.spbstu.ru>
2022-10-09 19:43:22 +0300

    - refactoring

--- recommendation_module/wsgi.py
+++ recommendation_module/wsgi.py
@@ -11,7 +11,7 @@ import os
 import asyncio
 
 from django.core.wsgi import get_wsgi_application
-# from async_kafka.views import main
+
 from async_kafka.views import main, consume_orders_data, consume_request_for_user
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'recommendation_module.settings')
@@ -20,8 +20,3 @@ application = get_wsgi_application()
 print("Server runs")
 
 asyncio.run(main())
-
-# asyncio.run(consume_request_for_user())
-# print("consume_request_for_user started")
-# asyncio.run(consume_orders_data())
-# print("consume_orders_data started")
