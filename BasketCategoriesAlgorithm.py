f5c7ee48242f48c5be66b497752dbfa0145548f5
Daniil Privalov <prival-chuk@mail.ru>
2022-10-26 12:30:12 +0300

    Refactoring

--- BasketCategoriesAlgorithm.py
+++ BasketCategoriesAlgorithm.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import json
 from pandas.io.json import json_normalize
-import numpy as np
+from BestProductAlgorithm import BestProductsAlgorithm
 
 
 class BasketCategoriesAlgorithm:
-    def __init__(self, products, orders, basket):
+    def __init__(self):
 
-        self.df_products = self.set_products(products)
-        self.df_customers = self.set_basket(basket)
-        self.df_orders = self.set_orders(orders)
+        self.df_products = self.data_frame_products()
+        self.df_customers = self.data_frame_customers()
+        self.df_orders = self.data_frame_orders()
 
         self.arr_of_order_combinations = []
         self.product_to_categories = {}
@@ -18,77 +18,43 @@ class BasketCategoriesAlgorithm:
         self.formula_result = []
         self.final_map = {}
         self.arr_recommendations_for_user = []
+        self.arr_recommendations_for_user_id = []
 
         self.arr_of_customer = []
 
-    # def data_frame_products(self):
-    #     with open("products.txt", "r", encoding='utf-8') as f:
-    #         data = json.load(f)
-    #         data = json_normalize(data)
-    #         return pd.DataFrame(data)
+    def data_frame_products(self):
+        with open("products.txt", "r", encoding='utf-8') as f:
+            data = json.load(f)
+            data = json_normalize(data)
+            return pd.DataFrame(data)
+    def data_frame_customers(self):
+        with open("customer.txt", "r", encoding='utf-8') as f:
+            data = json.load(f)
+            data = json_normalize(data)
+            return pd.DataFrame(data)
+    def data_frame_orders(self):
+        with open("orders.txt", "r", encoding='utf-8') as f:
+            data = json.load(f)
+            data = json_normalize(data)
+            return pd.DataFrame(data)
 
-    # def data_frame_customers(self):
-    #     with open("customer.txt", "r", encoding='utf-8') as f:
-    #         data = json.load(f)
-    #         data = json_normalize(data)
-    #         return pd.DataFrame(data)
-    #
-    # def data_frame_orders(self):
-    #     with open("orders.txt", "r", encoding='utf-8') as f:
-    #         data = json.load(f)
-    #         data = json_normalize(data)
-    #         return pd.DataFrame(data)
-
-    # Метод что б приводить файл с базы в нужный вид
-    def parsing(self, raw_str):
-        raw_str = raw_str.replace("{\'", "{\"")
-        raw_str = raw_str.replace(" \'", " \"")
-        raw_str = raw_str.replace("\':", "\":")
-        raw_str = raw_str.replace("\',", "\",")
-
-        raw_str = raw_str.replace("  \"non-filter_features\": {", " ")
-        raw_str = raw_str.replace("  },    \"filter_features\": { ", ",")
-        raw_str = raw_str.replace("    },    ", " ,")
-
-        return raw_str
-
-    def set_products(self, products):
-        print("DANIL STAFF")
-        raw_str = eval(products)
-        print("Danil raw_str")
-        print(raw_str)
-        raw_products = self.parsing(raw_str)
-        print("Danil raw_products" + raw_products)
-        print(type(raw_products))
-
-        data = json.loads(raw_products)
-        print("Final DF")
-        print(json_normalize(data))
-        return pd.DataFrame(json_normalize(data))
-
-    def set_orders(self, orders):
-        raw_orders = eval(orders)
-        raw_orders = self.parsing(raw_orders)
-        data = json.loads(raw_orders)
-        return pd.DataFrame(json_normalize(data))
-
-    def set_basket(self, basket):
-        raw_basket = eval(basket)
-        raw_basket = self.parsing(raw_basket)
-        data = json.loads(raw_basket)
-        return pd.DataFrame(json_normalize(data))
-
-     # Список заказов пользователей
     def get_products_from_customer(self) -> None:
+        """
+        Список заказов пользователей
+        :return: None
+        """
         for i in self.df_customers["products"][0]:
             self.arr_of_customer.append(i["id"])
 
         print("arr_of_customer:")
         print(self.arr_of_customer)
 
-    # Получаю все возможные комбинции из заказов причем двусторонние, т.е. [59, 88] и [88, 59] для удобной фильтрации
-    # в методе получения словаря
     def get_arr_of_order_combinations(self) -> None:
+        """
+        Получаю все возможные комбинции из заказов причем двусторонние, т.е. [59, 88] и [88, 59] для удобной фильтрации
+        в методе получения словаря
+        :return: None
+        """
         for i in range(len(self.df_orders)):
             temp1 = []
             for j in self.df_orders["products"][i]:
@@ -100,10 +66,12 @@ class BasketCategoriesAlgorithm:
         print("pairs_from_orders:")
         print(self.arr_of_order_combinations)
 
-
-    # Получаем словарь, где ключ это id товара, значение словарь с названиями категорий и количством товаров
-    # этой категории
     def get_product_to_categories(self) -> None:
+        """
+        Получаем словарь, где ключ это id товара, значение словарь с названиями категорий и количством товаров
+        этой категории
+        :return: None
+        """
         for i in self.arr_of_order_combinations:
 
             if not i[0] in self.product_to_categories:
@@ -117,19 +85,25 @@ class BasketCategoriesAlgorithm:
         print("product_to_categories")
         print(self.product_to_categories)
 
-    # Из предыдущего словаря получаем список всех возможных комбинаций, встречающихся в заказах
-    # который и будем прогонять через формулу
     def get_possible_combinations(self) -> None:
+        """
+        Из предыдущего словаря получаем список всех возможных комбинаций, встречающихся в заказах
+        который и будем прогонять через формулу
+        :return: None
+        """
         for i in self.product_to_categories.items():
             for j in i[1].items():
                 self.possible_combinations.append([i[0], j[0]])
         print("possible_combinations:")
         print(self.possible_combinations)
 
-    # Для применения формулы требуется количество появлений категории (товара) отдельно от другого. То есть при
-    # расчете знаменателя итоговой формулы нам нужно проходить не просто по спику заказов, а списку, из которого удлены
-    # заказы, содержащие рассматриваемую комбинацию товаров. Для этого нужен этот промежуточный массив
     def get_appearance_from_orders_separated(self) -> None:
+        """
+        Для применения формулы требуется количество появлений категории (товара) отдельно от другого. То есть при
+        расчете знаменателя итоговой формулы нам нужно проходить не просто по спику заказов, а списку, из которого удлены
+        заказы, содержащие рассматриваемую комбинацию товаров. Для этого нужен этот промежуточный массив
+        :return: None
+        """
         for i in range(len(self.df_orders)):
             temp1 = []
             for j in self.df_orders["products"][i]:
@@ -139,9 +113,12 @@ class BasketCategoriesAlgorithm:
         print("arr_of_appearance_from_orders:")
         print(self.arr_of_appearance_from_orders)
 
-    # Применяем формулу. Перед самой формулой выбрасываем из рассмотрения те заказы, в которых встретилась
-    # рассчитываемая пара товаров
     def use_formula(self) -> None:
+        """
+        Применяем формулу. Перед самой формулой выбрасываем из рассмотрения те заказы, в которых встретилась
+        рассчитываемая пара товаров
+        :return:
+        """
         for i in self.possible_combinations:
             temp_cleared_id = []
             temp_cleared_category = []
@@ -155,15 +132,17 @@ class BasketCategoriesAlgorithm:
                         temp_cleared_id.append(n)
                         temp_cleared_category.append(self.df_products["category"][n - 1])
 
-            # 0,1 доблены в сумму что б не получалось деления на 0
             value = self.product_to_categories[i[0]][i[1]] /(0.1 + temp_cleared_id.count(i[0]) + temp_cleared_category.count(i[1]))
             self.formula_result.append([i, value])
 
         print("formula_result")
         print(self.formula_result)
 
-    # Сравниваем полученные из формулы веса и оставляем только одну пару для каждого товара, получая финальный словарь
     def finalization(self) -> None:
+        """
+        Сравниваем полученные из формулы веса и оставляем только одну пару для каждого товара, получая финальный словарь
+        :return: None
+        """
         for i in self.formula_result:
             temp_array = i
             for j in self.formula_result:
@@ -178,15 +157,21 @@ class BasketCategoriesAlgorithm:
         print("sorted_map")
         print(sorted(self.final_map.items()))
 
-    # Метод принимает заказы пользователя и возварщает рекомендованные товары
     def get_recommendations_for_user(self, basket: list) -> list:
+        """
+        Метод принимает заказы пользователя и возварщает рекомендованные товары
+        :param basket: list of product id in the basket:
+        :return: arr of categories
+        """
         for i in basket:
             self.arr_recommendations_for_user.append(self.final_map.get(i))
-        print("SPECIAL FOR YOU!!!")
-        print(self.arr_recommendations_for_user)
         return self.arr_recommendations_for_user
 
     def do_basket_categories_algorithm(self):
+        """
+        Последовательное применение всех методов и получение рекоммендаций
+        :return: list of products id
+        """
         self.get_arr_of_order_combinations()
         self.get_product_to_categories()
         self.get_possible_combinations()
@@ -194,4 +179,13 @@ class BasketCategoriesAlgorithm:
         self.use_formula()
         self.finalization()
         self.get_products_from_customer()
-        self.get_recommendations_for_user(self.arr_of_customer)
+        result_1 = self.get_recommendations_for_user(self.arr_of_customer)
+        print("res1")
+        print(result_1)
+        bp = BestProductsAlgorithm()
+        result_2 = bp.do_best_product_algorithm()
+        print("res2")
+        print(result_2)
+        for i in result_1:
+            self.arr_recommendations_for_user_id.append(result_2[i][0])
+        return set(self.arr_recommendations_for_user_id)
--- BestProductAlgorithm.py
+++ BestProductAlgorithm.py
@@ -10,14 +10,6 @@ class BestProductsAlgorithm:
         self.map_of_categories = {}
         self.final_map = {}
 
-    # def data_frame_products(self) :
-    #     with open("products.txt", "r", encoding='utf-8') as f:
-    #         data = json.load(f)
-    #         data = json_normalize(data)
-    #         print("Final hueta 1")
-    #         print(data)
-    #         return pd.DataFrame(data)
-
     def parsing(self, raw_str):
         raw_str = raw_str.replace("{\'", "{\"")
         raw_str = raw_str.replace(" \'", " \"")
@@ -25,7 +17,7 @@ class BestProductsAlgorithm:
         raw_str = raw_str.replace("\',", "\",")
         raw_str = raw_str.replace("},  ,", "},")
 
-        raw_str = raw_str.replace("  \"non-filter_features\": {", " ")
+        raw_str = raw_str.replace("  \"non_filter_features\": {", " ")
         raw_str = raw_str.replace("  },    \"filter_features\": { ", ",")
         raw_str = raw_str.replace("    },    ", " ,")
 
@@ -45,20 +37,23 @@ class BestProductsAlgorithm:
         print(json_normalize(data))
         return pd.DataFrame(json_normalize(data))
 
-    # Получаем словарь словарей, в котором ключем будут названия категории, значем - словарь с ключем id товара
-    # и значением рейтинга
     def get_product_to_categories(self):
+        """
+        Получаем словарь словарей, в котором ключем будут названия категории, значем - словарь с ключем id товара
+        и значением рейтинга
+        :return: None
+        """
         for i in range(len(self.df_products)):
             if not self.df_products["category"][i] in self.map_of_categories:
                 self.map_of_categories[self.df_products["category"][i]] = dict([])
 
             self.map_of_categories[self.df_products["category"][i]][self.df_products["id"][i]] = self.df_products["rating"][i]
 
-        print("KYFYGKSUEFUYKUEmap_of_categories")
-        print(self.map_of_categories)
-
-    # Сортируем по убыванию оценок и записывааем в новый словарь только 2 лучших товара из каждой ктегории
     def sort_and_get_final(self):
+        """
+        Сортируем по убыванию оценок и записывааем в новый словарь только 2 лучших товара из каждой ктегории
+        :return: dict of recommendations
+        """
         for i in self.map_of_categories.items():
             temp1 = sorted(i[1].items(), key=lambda x: x[1], reverse=True)
             temp2 = []
@@ -67,9 +62,14 @@ class BestProductsAlgorithm:
             self.final_map[i[0]] = temp2
         print("final_map")
         print(self.final_map)
-        return list(self.final_map)
+        return self.final_map
 
     def do_best_product_algorithm(self):
+        """
+        Последовательное применение всех методов и получение рекоммендаций
+        :return: dict of recommendations
+        """
         self.get_product_to_categories()
-        self.sort_and_get_final()
+        result = self.sort_and_get_final()
+        return result
 
